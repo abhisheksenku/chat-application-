c5b98fbaf7f99c183eb6dbfb0c2f266d04c2ee74
abhisheksenku <abhi971255@gmail.com>
2025-08-26 12:01:49 +0530

    login backend plus frontend

---
+++ models/association.js
@@ -0,0 +1,4 @@
+const User = require('../models/users');
+module.exports = {
+    User
+}
---
+++ public/js/login.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit',async (event)=>{
+        event.preventDefault();
+        const formData = new FormData(loginForm);
+        const formValues = Object.fromEntries(formData.entries());
+        console.log(formValues);
+        try {
+            const response = await axios.post(`${BASE_URL}/user/login`,formValues);
+            console.log('Login successful',response.data);
+            localStorage.setItem('token',response.data.token);
+            loginForm.reset();
+        } catch (error) {
+            console.error('Error while loggin in:',error);
+            alert('Invalid email or password')   
+        }
+    })
+})
